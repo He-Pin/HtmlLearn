729cd637414dd5fdf3ea323ee3f13a07ea2c840a
hepin1989 <hepin1989@gmail.com>
2013-07-12 20:30:50 +0800

    add javascript

---
+++ javascript/implement/js/myFunction.js
@@ -0,0 +1,14 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: pekall
+ * Date: 7/12/13
+ * Time: 2:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+function func() {
+    document.getElementById("demo").innerHTML = "My first javascript func in another file";
+}
+
+function funcVar(id) {
+    document.getElementById(id).innerHTML = "my first javascript func with var in another file";
+}
